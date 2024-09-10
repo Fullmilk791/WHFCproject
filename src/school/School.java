4abee8a9a1e3925ac78846739db188341bd7beb7
amansandford <amansandford6@gmail.com>
2024-09-10 21:00:14 +0300

    init commit

---
+++ src/school/School.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package school;
+
+/**
+ *
+ * @author amans
+ */
+public class School {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login2 LoginFrame;
+        LoginFrame = new login2();
+        LoginFrame.setVisible(true);
+        LoginFrame.setLocationRelativeTo(null);
+                
+        // TODO code application logic here
+    }
+    
+}
